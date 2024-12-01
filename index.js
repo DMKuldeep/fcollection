a6f2f91e10156110fbc848c927eda4f59a07cfbd
Kuldeep tripathi <tkuldeep271@gmail.com>
2024-12-01 01:20:43 -0500

    combined pages

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ document.querySelectorAll('.position-relative').forEach(container => {
     video.currentTime = 0; // Reset video to start
   });
 });
+
+
+const icons = document.querySelectorAll('.icon');
+const contents = document.querySelectorAll('.collections');
+
+icons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        const targetId = icon.getAttribute('data-target');
+        
+        // Hide all content divs
+        contents.forEach(content => content.classList.remove('active'));
+        
+        // Show the selected content div
+        document.getElementById(targetId).classList.add('active');
+    });
+});
